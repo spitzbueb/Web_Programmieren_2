7e76bf4788c66db34232054bbdc549c040f4aba1
Philipp Schalcher <philipp.schalcher@phischa.ch>
2012-03-27 13:42:58 +0200

    Kapitel 7.1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url,presence:true
+	validates :price,numericality:{greater_than_or_equal_to:0.05}
+	validates :title,uniqueness:true
+	validates :image_url,allow_blank:true,format:{
+		with: %r{\.(gif|png|jpg)$}i, message:'must be an URL for GIF, JPG or PNG'
+		}
 end
