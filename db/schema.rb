2d6231e3249df5684faeb35e8b85eeff774e3249
Philipp Schalcher <philipp.schalcher@phischa.ch>
2012-05-29 14:12:49 +0200

    Fehler behoben, falsche version

--- db/schema.rb
+++ db/schema.rb
@@ -21,18 +21,18 @@ ActiveRecord::Schema.define(:version => 20120525070019) do
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer  "quantity"
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.decimal  "price",       :precision => 8, :scale => 2
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
   end
 
 end
