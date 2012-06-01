fa2ce71d4d1b0cfd910a5d9bc6561a82e2720109
Philipp Schalcher <philipp.schalcher@phischa.ch>
2012-06-01 10:38:02 +0200

    Fehler mit Version auf Heroku behoben

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,7 +6,6 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-#...
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
